f3ff50f198ccf5eaff0762aabfb172812884e6cd
Denis <mikulaj.d@gmail.com>
2024-11-26 15:40:25 +0100

    update

--- zadanie-1/index.php
+++ zadanie-1/index.php
@@ -3,16 +3,17 @@ define("LOG_FILE", "datetime_log.txt");
 function logDate(DateTime $arrivalDateTime, bool $isLate)
 {
     $formattedDate =  $arrivalDateTime->format("D d. F Y - H:i:s") . ($isLate ? " - meskanie" : "");
-    //echo $formattedDate . PHP_EOL;
+    echo "Aktualny prichod: " . $formattedDate . "<br><br><br>";
     file_put_contents(LOG_FILE, $formattedDate . PHP_EOL, FILE_APPEND);
 }
 
 function getLogs()
 {
+    echo "Vsetky prichody: <br>";
     $file =  fopen(LOG_FILE, "r");
     if ($file) {
         while (($line = fgets($file, 4096)) !== false) {
-            echo nl2br($line);
+            echo  $line . "<br>";
         }
     }
     fclose($file);
@@ -26,12 +27,14 @@ function studentArrival()
     $minutes = intval($currentDate->format("i"));
     $seconds = intval($currentDate->format("s"));
     $timeInSeconds = $hours * 60 * 60 + $minutes * 60 + $seconds;
-    $isLate = $timeInSeconds > 8 * 60 * 60;
     if ($timeInSeconds >= 20 * 60 * 60 && $timeInSeconds <= 24 * 60 * 60) {
         die("Prichody v casoch medzi 20:00 a 24:00 nie je mozne zapisat");
     }
+    $isLate = $timeInSeconds > 8 * 60 * 60;
+
     logDate($currentDate, $isLate);
-} ?>
+}
+?>
 
 <!DOCTYPE html>
 <html>

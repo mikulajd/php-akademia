ac8e505d8d8d05920fd949b92a6c6510868237cd
Denis <mikulaj.d@gmail.com>
2024-11-29 00:26:36 +0100

    cleaning up

--- zadanie-2/index.php
+++ zadanie-2/index.php
@@ -10,7 +10,7 @@ require_once 'arrival.php';
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Prichody studentov</title>
+    <title>Príchody študentov</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
@@ -26,7 +26,6 @@ require_once 'arrival.php';
         </form>
 
         <?php
-        $showError = false;
         $arrivals = new Arrivals();
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             if (!empty($_POST['name']) && isset($_POST['addStudent'])) {
